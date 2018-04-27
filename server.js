2f76af8af64e27af4ed85ad13f0bee4012afb70e
Ishita Jaju <36659157+theishita@users.noreply.github.com>
2018-04-28 01:50:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function(req, res, next) {  
+app.get('/main.js', function(req, res, next) {  
     console.log("before redirection");
     res.sendfile('index.html'); 
 });
