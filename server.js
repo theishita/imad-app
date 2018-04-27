c52a17238d1ae414388f18d0d4dd9de5f8331b7a
Ishita Jaju <36659157+theishita@users.noreply.github.com>
2018-04-28 01:51:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,9 +81,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/main.js', function(req, res, next) {  
-    console.log("before redirection");
-    res.sendfile('index.html'); 
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var pool = new Pool(config);
