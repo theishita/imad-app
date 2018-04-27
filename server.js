ed8f2ce88cb52f6570d90aaca2988ed8936cf1bb
Ishita Jaju <36659157+theishita@users.noreply.github.com>
2018-04-28 02:37:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/ui/main.js', function (req, res) {
 var names=[];
 app.get('/submit-name/:name', function(req,res){
    //get name from the request
-   var name = req.params.name;
+   var name = req.query.name;
    
    names.push(name);
    //use JSON
