44ec9bc743795ae58db25e3032443e21f91b11a4
Ishita Jaju <36659157+theishita@users.noreply.github.com>
2018-04-28 02:08:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,21 @@
-alert('Hi!');
+//counter
+var button = document.getElementById('counter');
+button.onclick = function(){
+    //create a request object
+    var request = XMLHttpRequest();
+    
+    //capture response and store in a var
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    //make the request
+    request.open('GET','http://ishitajaju2016.imad.hasura-app.io/counter',true);
+    request.send(null);
+}
