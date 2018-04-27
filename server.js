287c8632f7649158e7cf1b145be07cbae00823ad
Ishita Jaju <36659157+theishita@users.noreply.github.com>
2018-04-28 03:41:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,33 +21,6 @@ app.get('/', function (req, res) {
 
 app.use(express.static('ui'));
 
-var articles = {
-    'article-one': {
-      title: 'Article One',
-      heading: 'Article One',
-      date: 'Mar 18, 2018',
-      content: `
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                </p>`
-    },
-    'article-two': {
-      title: 'Article Two',
-      heading: 'Article Two',
-      date: 'Mar 19, 2018',
-      content: `
-                <p>
-                    This is the content for my second article.
-                </p>`
-    }
-};
-
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
