aa0c6c7e34f5066038e5eada0ea01894958ac98a
Ishita Jaju <36659157+theishita@users.noreply.github.com>
2018-04-28 03:27:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,8 +127,6 @@ app.get('/articles/:articleName',function(req, res){
             }
         }
     });
-
-  res.send(createTemplate(articleData));
 });
 
 app.get('/ui/style.css', function (req, res) {
