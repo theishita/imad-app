7450953f9f729efe0b0b073fb8fe69fbb34447c8
theishita <36659157+theishita@users.noreply.github.com>
2018-04-27 17:59:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 
-/*var config = {
+var config = {
     user: 'ishitajaju2016',
     database: 'ishitajaju2016',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env,DB_PASSWORD
+    password: 'db-ishitajaju2016-12905'
 }
-*/
+
 var app = express();
 app.use(morgan('combined'));
 
