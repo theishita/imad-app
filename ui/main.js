0108854a16f06f3ccebeb977804b813dc220f26e
Ishita Jaju <36659157+theishita@users.noreply.github.com>
2018-04-28 01:17:47 +0530

    Create main.js

---
+++ ui/main.js
@@ -0,0 +1,11 @@
+console.log('Loaded!');
+
+//change value of main text
+var element = document.getElementById('main-txt');
+element.innerHTML = 'New value';
+
+//move image on clicking
+var img=document.getElementById('cat');
+img.onclick = function() {
+    img.style.marginleft = '100px';
+}
